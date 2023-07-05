757b503238bffecb299e4a43f73029dada93183d
Emanuel TRINC <emanuel.trinc@hoprnet.org>
2023-07-05 15:58:55 +0300

    TAF setup

--- src/test_plans/integration/test_api.py
+++ src/test_plans/integration/test_api.py
@@ -3,6 +3,8 @@ import time
 import swagger_client
 from swagger_client.api import AccountApi
 from swagger_client.rest import ApiException
+from swagger_client.models import AccountWithdrawBody
+from swagger_client.models.currency import Currency
 from ...target_environment.anvil_env_manager import AnvilEnvironmentManager
 
 
@@ -12,6 +14,41 @@ Send Messages Test Plan Implementation.
 
 TOKEN = "e2e-API-token^^"
 HOST = "localhost"
+
+def get_balance(node_index: int, currency: Currency):
+    """
+    Fetch and return the balance of a certain given node index.
+    """
+    configuration = swagger_client.Configuration()
+    configuration.api_key["x-auth-token"] = TOKEN
+    configuration.host = f"http://{HOST}:1330{node_index}/api/v2"
+    api_instance = swagger_client.AccountApi(swagger_client.ApiClient(configuration))
+    try:
+        api_response = api_instance.account_get_balances()
+        if currency == Currency.NATIVE:
+            return api_response.native
+        if currency is Currency.HOPR:
+            return api_response.hopr
+    except ApiException as e:
+        print("Exception when calling AccountApi->account_get_addresses: %s\n" % e)
+
+def withdraw(currency: Currency, node_index: int, amount: int, address: str):
+    """
+    """
+    configuration = swagger_client.Configuration()
+    configuration.api_key["x-auth-token"] = TOKEN
+    configuration.host = f"http://{HOST}:1330{node_index}/api/v2"
+    api_instance = swagger_client.AccountApi(swagger_client.ApiClient(configuration))
+    try:
+        body = swagger_client.AccountWithdrawBody(currency, amount, address)
+
+        api_response = api_instance.account_withdraw(body=body)
+        balance_native = api_response.native
+        balance_hopr = api_response.hopr
+        assert balance_native != 0
+        assert balance_hopr != 0
+    except ApiException as e:
+        print("Exception when calling AccountApi->account_get_addresses: %s\n" % e)
 
 def validate_native_address(node_index: int):
     """ """
@@ -32,7 +69,6 @@ def validate_hopr_address(address: str):
     """ """
     pass
 
-
 def validate_balance(node_index: int):
     """
     Validate that node is funded.
@@ -45,22 +81,25 @@ def validate_balance(node_index: int):
         api_response = api_instance.account_get_balances()
         balance_native = api_response.native
         balance_hopr = api_response.hopr
-        f = open("log.txt", "w")
-        f.write("native: " + balance_native + " hopr: " + balance_hopr + "\n")
-        f.write(str(api_response))
-        f.flush()
-        f.close()
         assert balance_native != 0
         assert balance_hopr != 0
     except ApiException as e:
         print("Exception when calling AccountApi->account_get_addresses: %s\n" % e)
 
+def check_withdraw(node_index: int, currency: Currency, previous_balance):
+    """
+    """
+    native_balance = get_balance(currency)
+
+
+def check_hopr_withdraw():
+    """
+    """
 
 def test_case1():
     """ """
     anvil_env_manager = AnvilEnvironmentManager()
     anvil_env_manager.setup_local_nodes(1)
-    logging.critical("looooggg")
     #validate_native_address(1)
     # validate_native_address(2)
     # validate_native_address(3)
@@ -72,3 +111,12 @@ def test_case1():
     # validate_balance(3)
     # validate_balance(4)
     # validate_balance(5)
+
+    current_balance_native = get_balance(1, Currency.NATIVE)
+    current_balance_hopr = get_balance(1, Currency.HOPR)
+
+    withdraw(Currency.NATIVE, 10, '0x858aa354db6ae5ea1217c5018c90403bde94e09e')
+    withdraw(Currency.HOPR, 10, '0x858aa354db6ae5ea1217c5018c90403bde94e09e')
+
+    check_native_withdraw()
+    check_hopr_withdraw()
