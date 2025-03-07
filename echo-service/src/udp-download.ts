a37a144c3977246d269b4c715ddbe04c70c261a1
ausias-armesto <ausiasarmesto@gmail.com>
2025-03-07 16:02:05 +0100

    Only debug 1 route

--- echo-service/src/udp-download.ts
+++ echo-service/src/udp-download.ts
@@ -19,41 +19,47 @@ socketServer.on('message', (message, remoteInfo) => {
     let dataSent = 0;
     const startTime = Date.now();
 
-        try{
-            const downloadSettings = JSON.parse(downloadSettingsString.toString());
-            const payloadSize = downloadSettings.payloadSize; // Data in bytes
-            const segmentSize = downloadSettings.segmentSize; // Data in bytes
-            const throughput = downloadSettings.throughput; // bytes/s
-            const segmentsPerSecond = throughput / segmentSize;
-            const payloadSizeMB = (payloadSize / (1024 * 1024 )).toFixed(2); // MB
-            const throughputMB = (throughput / (1024 * 1024)).toFixed(2); // MB/s
-            let segmentsSent = 0;
+    try{
+        const downloadSettings = JSON.parse(downloadSettingsString.toString());
+        const payloadSize = downloadSettings.payloadSize; // Data in bytes
+        const segmentSize = downloadSettings.segmentSize; // Data in bytes
+        const throughput = downloadSettings.throughput; // bytes/s
+        const segmentsPerSecond = throughput / segmentSize;
+        const payloadSizeMB = (payloadSize / (1024 * 1024 )).toFixed(2); // MB
+        const throughputMB = (throughput / (1024 * 1024)).toFixed(2); // MB/s
+        let segmentsSent = 0;
+        if (downloadSettings.sessionPath === 'node-1 => node-2 => node-5') {
             console.log(`[UDP][Download] Downloading ${payloadSizeMB} MB at ${throughputMB} MB/s ( ${segmentsPerSecond.toFixed(0)} segments/s) over UDP using session path ${downloadSettings.sessionPath}`);
-            const interval = setInterval(() => {
-                    const dataChunk = randomBytes(segmentSize);
-                    if (dataSent < payloadSize) {
-                        socketServer.send(dataChunk, remoteInfo.port, remoteInfo.address, (err) => {
-                            if (err) {
-                                console.error('[UDP][Download] Error sending data:', err.message);
-                                clearInterval(interval);
-                                return;
-                            }
-                            dataSent += dataChunk.length;
-                            segmentsSent++;
-                            //console.log(`[UDP][Download] Sent ${segmentsSent} segments with ${dataSent} bytes in total.`);
-                        })
-                    } else {
-                        clearInterval(interval);
-                        const endTime = Date.now();
-                        const duration = (endTime - startTime) / 1000; // In seconds
-                        const speed = ((payloadSize) / duration) / (1024 * 1024); // MB/s
+        }
+        const interval = setInterval(() => {
+                const dataChunk = randomBytes(segmentSize);
+                if (dataSent < payloadSize) {
+                    socketServer.send(dataChunk, remoteInfo.port, remoteInfo.address, (err) => {
+                        if (err) {
+                            console.error('[UDP][Download] Error sending data:', err.message);
+                            clearInterval(interval);
+                            return;
+                        }
+                        dataSent += dataChunk.length;
+                        segmentsSent++;
+                        if (downloadSettings.sessionPath === 'node-1 => node-2 => node-5') {
+                            console.log(`[UDP][Download] Sent ${segmentsSent} segments with ${dataSent} bytes in total.`);
+                        }
+                    })
+                } else {
+                    clearInterval(interval);
+                    const endTime = Date.now();
+                    const duration = (endTime - startTime) / 1000; // In seconds
+                    const speed = ((payloadSize) / duration) / (1024 * 1024); // MB/s
+                    if (downloadSettings.sessionPath === 'node-1 => node-2 => node-5') {
                         console.log(`[UDP][Download] Download finished: ${payloadSizeMB} MB sent in ${duration.toFixed(2)} seconds (${speed.toFixed(2)} MB/s) using session path ${downloadSettings.sessionPath}`);
                     }
-            }, 1000 / segmentsPerSecond );
-        } catch (err) {
-            console.error(`[UDP][Download] Error handling message from ${remoteInfo.address}:${remoteInfo.port}`);
-            console.error("[UDP][Download] Error parsing download settings or sending data:", err);
-        }
+                }
+        }, 1000 / segmentsPerSecond );
+    } catch (err) {
+        console.error(`[UDP][Download] Error handling message from ${remoteInfo.address}:${remoteInfo.port}`);
+        console.error("[UDP][Download] Error parsing download settings or sending data:", err);
+    }
 });
 
 // Handle socket errors
